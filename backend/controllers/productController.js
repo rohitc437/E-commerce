4c8c78909569887b73a89fa699c2298d11bd2d76
Rohit <rohitc437>
2022-02-13 12:25:01 +0530

    create,update product added routes and controller

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,6 @@
 const Product = require("../models/productModel");
 
+//create product by admin
 exports.createProduct = async (req, res) => {
   const product = await Product.create(req.body);
   res.status(201).json({
@@ -8,7 +9,30 @@ exports.createProduct = async (req, res) => {
   });
 };
 
+//get all Products
 exports.getAllProducts = async (req, res) => {
   const products = await Product.find();
   res.status(200).json({ success: true, products });
+};
+
+//update product by admin
+exports.updateProduct = async (req, res, next) => {
+  console.log(req.params.id);
+  let product = await Product.findById(req.params.id);
+  if (!product) {
+    res.status(500).json({
+      success: false,
+      message: "Product Not Found",
+    });
+  }
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
 };
