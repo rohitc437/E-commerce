f6edbe9706e923cfabbff7af91ac5145ae8dbf80
Rohit <rohitc437>
2022-02-20 20:19:57 +0530

    Order API Donegit add .

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,18 @@
 const express = require("express");
 const app = express();
-const cookieParser = require('cookie-parser')
+const cookieParser = require("cookie-parser");
 const ErrorMiddleware = require("./middleware/error");
 app.use(express.json());
-app.use(cookieParser())
-
+app.use(cookieParser());
 
 //routes imports
 const productRoute = require("./routes/productRoutes");
 const userRoute = require("./routes/userRoutes");
+const orderRoute = require("./routes/orderRoutes");
 
 app.use("/api/v1", productRoute);
 app.use("/api/v1", userRoute);
-
-
+app.use("/api/v1", orderRoute);
 
 //Middleware for Error
 app.use(ErrorMiddleware);
